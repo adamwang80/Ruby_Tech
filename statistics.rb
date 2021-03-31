df4f5b4f3101cbbabb960fb621388e64064a3b80
Adam Wang <adamywang80@gmail.com>
2021-03-31 12:15:54 -0400

    add a line of comment

--- statistics.rb
+++ statistics.rb
@@ -3,3 +3,4 @@ require 'statistics'
 anova = StatisticalTest::FTest.one_way_anova(alpha = 0.05, [1, 2, 3], [4, 5, 6], [7, 8, 9]);
 
 puts anova
+# => {:probability=>0.999, :p_value=>0.0010000000000000009, :alpha=>0.05, :null=>false, :alternative=>true, :confidence_level=>0.95}

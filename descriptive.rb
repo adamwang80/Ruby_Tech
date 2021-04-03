60dfac925b3fc7e9ff5e0245ac39f59a62e991e1
Victoria Hall <victoria.hall20@yahoo.com>
2021-04-02 23:42:01 -0400

    added your turn for descriptive

--- descriptive.rb
+++ descriptive.rb
@@ -1,4 +1,6 @@
 require 'descriptive_statistics'
+
+# Example Given
 data = [2,6,9,3,5,1,8,3,6,9,2]
 # => [2, 6, 9, 3, 5, 1, 8, 3, 6, 9, 2]
 data.number
@@ -24,3 +26,16 @@ data.mode
 data.range
 # => 8
 
+# Your Turn Example
+newdata = [0, 5, 10, 15] # => [0, 5, 10, 15]
+newdata.number # => 4.0
+newdata.sum # => 30
+newdata.mean # => 7.5
+newdata.median # => 7.5
+#puts newdata.variance # => 31.25
+#puts newdata.standard_deviation # => 5.5901699437494745
+#puts newdata.percentile(30) # => 4.5
+#puts newdata.percentile(70) # => 10.499999999999998
+#puts newdata.percentile_rank(8) # => 50.0
+newdata.mode # => 0
+newdata.range # => 15.0
